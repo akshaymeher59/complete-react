50bd1e67ca680cf1c2fe465094ccf4abf19e3411
Akshay Meher <meherakshay59@gmail.com>
2023-06-21 19:03:42 +0530

    Rendering Lists of Data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,12 +16,35 @@ const filterChangeHandler=(selectedYear)=>{
 
     return (
         <div>
-            <Card 
-            className="expenses">
+            <Card className="expenses">
 
                 <ExpensesFilter 
-                selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-                <ExpenseItem
+                selected={filteredYear} 
+                onChangeFilter={filterChangeHandler}
+                />
+
+                {
+                    props.items.map( expense => 
+                    <ExpenseItem 
+                        title={ expense.title}
+                        amount ={expense.amount}
+                        date = {expense.date}
+                    /> )
+                }
+
+                {/* Alternative Arrow syntax
+                {
+                    props.items.map( (expense) =>{
+                        return ( 
+                        <ExpenseItem 
+                            title={ expense.title}
+                            amount ={expense.amount}
+                            date = {expense.date}
+                        />)
+                    })
+                } */}
+
+                {/* <ExpenseItem
                     title={props.items[0].title}
                     amount={props.items[0].amount}
                     date={props.items[0].date}
@@ -40,10 +63,11 @@ const filterChangeHandler=(selectedYear)=>{
                     title={props.items[3].title}
                     amount={props.items[3].amount}
                     date={props.items[3].date}
-                />
+                /> */}
             </Card>
         </div>
     )
 }
 
 export default Expenses;
+
