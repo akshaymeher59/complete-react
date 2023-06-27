f8994f884ba6b21e8a847f7427a9a21b9bc55388
Akshay Meher <meherakshay59@gmail.com>
2023-06-27 10:22:24 +0530

    Adding Conditional Return Statements

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,22 +6,18 @@ import Card from '../UI/Card';
 
 function ExpenseItem(props) {
 
-//  const [title, setTitle] = useState( props.title );
-  
-  // const clickHandler= ()=>{
-  //   setTitle('updated');
-  // }
-  
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
 
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-    {/* <button  > Change</button> */}
-    </Card>
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+
+      </Card>
+    </li>
   );
 }
 
---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './ExpensesList.css'
+import ExpenseItem from './ExpenseItem';
+
+const ExpensesList = ( {filteredExpenses} ) => {
+    
+
+    if ( filteredExpenses.length === 0 ) {
+        return <h2 className='expenses-list__fallback'>Found no Expenses.</h2>
+    }
+  return (
+    <ul className='expenses-list'>
+        {
+            filteredExpenses.map((expense) => (
+                <ExpenseItem
+                    key={expense.id}
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date}
+                />
+            ))
+        }
+    </ul>
+  )
+}
+
+export default ExpensesList
